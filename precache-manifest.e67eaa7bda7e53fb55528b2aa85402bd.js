21f2d8d575e661fe057905733912450956e39e68
Code Nation 13 <codenation@CN-13.local>
2019-03-27 17:36:53 +0000

    Updates

---
+++ precache-manifest.e67eaa7bda7e53fb55528b2aa85402bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06565e9e0922560c32db",
+    "url": "/static/js/main.4e20d99a.chunk.js"
+  },
+  {
+    "revision": "b05778cad4e7a5ef29c8",
+    "url": "/static/js/2.c8678dea.chunk.js"
+  },
+  {
+    "revision": "06565e9e0922560c32db",
+    "url": "/static/css/main.b9bfe4a9.chunk.css"
+  },
+  {
+    "revision": "b00fa0361d52e3678d2aacf699dfc46a",
+    "url": "/index.html"
+  }
+];
