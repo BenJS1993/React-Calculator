6be6c45cf7a07cb635caabb6ac5515bd0535e1d2
Code Nation 13 <codenation@CN-13.local>
2019-03-27 18:01:48 +0000

    Updates

---
+++ precache-manifest.b0cfa31972fe8b1b26b6e699b54d48c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19a8db08324e9b1cabe9",
+    "url": "/React-Calculator/static/js/runtime~main.cb778861.js"
+  },
+  {
+    "revision": "8a5b6c58ac3b0ef20956",
+    "url": "/React-Calculator/static/js/main.8717f927.chunk.js"
+  },
+  {
+    "revision": "b05778cad4e7a5ef29c8",
+    "url": "/React-Calculator/static/js/2.c8678dea.chunk.js"
+  },
+  {
+    "revision": "8a5b6c58ac3b0ef20956",
+    "url": "/React-Calculator/static/css/main.b9bfe4a9.chunk.css"
+  },
+  {
+    "revision": "53231dcf599cd7ea323631353dc27577",
+    "url": "/React-Calculator/index.html"
+  }
+];
